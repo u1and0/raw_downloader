cf56e8c8c8edcf1b6986025405a8d0c54a0c879b
u1and0 <e01.ando60@gmail.com>
2024-08-07 06:54:29 +0900

    [docs] CLI options

--- raw_downloader.py
+++ raw_downloader.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
 """
-Usage:
-$ python raw_downloader.py
+Mangakoma downloader
 
-スキップする話数、出力ディレクトリを調整する。
+https://mangakoma.org/ の漫画をPDFとしてダウンロードします。
+
+Usage:
+    $ python raw_downloader.py [-h] [-v] [-o OUTPUT] [-s SKIP] [-d DRIVER] url
+    $ python raw_downloader.py --output=/path/of/output --skip=5\
+    --driver=/usr/bin/chromedriver https://mangakoma.org/manga/title/di-1hua
+
+Options:
+    url: 取得元URL。話数はどの話数からでも良い。話数セレクトボックスから得られるすべての話数を1話から順に取得する。
+    --output: 出力先ディレクトリ。(default: カレントディレクトリ)
+    --skip: スキップする話数。例として、5を指定すると6話からダウンロードする。(default: 0)
+    --driver: chrome driver path(default: /usr/bin/chromedriver)
 
 Install:
 $ yay -Sy chromedriver
 $ conda install -yc conda-forge selenium
 $ yay -S google-chrome
-
-chromiumをインストールしたが、うまくいかなかった。
-オプションやサービスの指定の仕方がわからなかった
 """
 
 import os
 import time
 from typing import Optional
 import tempfile
+import argparse
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
+
+VERSION = "v0.1.0"
 
 
 def find_jpg_source(soup: BeautifulSoup) -> list[str]:
@@ -47,7 +57,7 @@ def fetch_image(url: str, save_dir: str) -> Optional[str]:
 
 
 def download_images(links: list[str]) -> list[str]:
-    """渡されたURLリストのコンテンツをカレントディレクトリに保存する"""
+    """渡されたURLリストのコンテンツを一時保管のディレクトリに保存する"""
     downloaded_imgs = []
     tempdir = tempfile.mkdtemp()
     for link in links:
@@ -77,9 +87,17 @@ def get_story_urls(content: BeautifulSoup) -> list[str]:
 class RawDownloader:
 
     def __init__(self, chromedriver_path: str = "/usr/bin/chromedriver"):
+        """ダウンローダーの初期化
+        chromedriverのパスを設定する
+        """
         self.chromedriver_path = chromedriver_path
 
     def download(self, url: str, out_dir: str, skip_file_num: int = 0):
+        """
+        1. セレクトボックスから取得先URLをすべて取得する
+        2. 1話ずつ_fetch_content_create_pdf()へ渡す
+        3. 画像取得してPDF化する
+        """
         try:
             # out_dir が見つからなければディレクトリ作成
             os.makedirs(out_dir, exist_ok=False)
@@ -101,7 +119,7 @@ class RawDownloader:
             self._fetch_content_create_pdf(url, out_dir)
 
     def create_driver(self) -> webdriver.Chrome:
-        # seleniumで扱うchromeドライバを生成する
+        """seleniumで扱うchromeドライバを生成する"""
         # chromedriver_path が見つからなければ終了
         if not os.path.isfile(self.chromedriver_path):
             raise FileNotFoundError(self.chromedriver_path)
@@ -145,10 +163,53 @@ class RawDownloader:
         return driver.page_source
 
 
+def parse() -> argparse.Namespace:
+    """コマンドライン引数の解釈"""
+    parser = argparse.ArgumentParser(
+        description="https://mangakoma.org/ の漫画をPDFとしてダウンロードします。")
+    parser.add_argument(
+        "url",
+        type=str,
+        help="取得元URL。話数はどの話数からでも良い。話数セレクトボックスから得られるすべての話数を1話から順に取得する。",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {VERSION}",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default=os.getcwd(),
+        help="出力先ディレクトリ。(default: カレントディレクトリ)",
+    )
+    parser.add_argument(
+        "-s",
+        "--skip",
+        type=int,
+        default=0,
+        help="スキップする話数。例として、5を指定すると6話からダウンロードする。(default: 0)",
+    )
+    parser.add_argument(
+        "-d",
+        "--driver",
+        type=str,
+        default="/usr/bin/chromedriver",
+        help="chrome driver path(default: /usr/bin/chromedriver)",
+    )
+    return parser.parse_args()
+
+
+def main():
+    # コマンドライン引数の解釈
+    args = parse()
+    # ダウンローダーの初期化
+    raw = RawDownloader(args.driver)
+    # PDFのダウンロード
+    raw.download(args.url, args.output)
+
+
 if __name__ == "__main__":
-    # SKIPPED = -134  # すでにダウンロード済みで、スキップする話数
-    raw = RawDownloader()
-    out_dir = "/mnt/e/Users/U1and0/Documents/PDF/呪術廻戦"
-    # 全話のURLを取得するためのURL, どの話数でもいい
-    url = "https://mangakoma01.net/manga/zhou-shu-hui-zhana004/di1hua"
-    raw.download(url, out_dir)
+    main()
